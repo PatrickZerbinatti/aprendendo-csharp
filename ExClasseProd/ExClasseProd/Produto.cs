20b198d345e4dc49771ae83848338e73cd290447
Patrick Geovani <patrick.geovani357@hotmail.com>
2020-06-15 13:35:12 -0300

    add ex

---
+++ ExClasseProd/ExClasseProd/Produto.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExClasseProd
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Qtd;
+
+        public Produto(string nome, double preco, int qtd)
+        {
+            Nome = nome;
+            Preco = preco;
+            Qtd = qtd;
+        }
+
+        public override string ToString()
+        {
+            return "Dados do produto: " 
+                + Nome 
+                + ", $" 
+                + Preco.ToString("F2") 
+                + ", " 
+                + Qtd 
+                + " unidades, Total: $" 
+                + ValorTotalEmEstoque().ToString("F2");
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Qtd;
+        }
+
+        public void AdicionarProduto(int quant)
+        {
+            Qtd += quant;
+        }
+
+        public void RemoverProduto(int quant)
+        {
+            Qtd -= quant;
+        }
+
+    }
+}
---
+++ ExClasseProd/ExClasseProd/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ExClasseProd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto produto;
+            string nome;
+            double preco;
+            int qtd,aux = 1;
+
+            Console.WriteLine("Digite o nome do produto:");
+            nome = Console.ReadLine();
+            Console.WriteLine("Digite o preço do produto:");
+            preco = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o qtd do produto:");
+            qtd = int.Parse(Console.ReadLine());
+
+            produto = new Produto(nome, preco, qtd);
+
+            while (aux != 0)
+            {
+                Console.WriteLine(produto);
+
+                Console.WriteLine("Opções: 0 = Sair , 1 = Adicionar Produto , 2 = Remover Produto");
+
+                aux = int.Parse(Console.ReadLine());
+
+                if( aux == 1){
+
+                    Console.Write("Digite o numero de produtos a serem adicionados ao estoque: ");
+                    produto.AdicionarProduto(int.Parse(Console.ReadLine()));
+
+                }else if( aux == 2){
+
+                    Console.Write("Digite o numero de produtos a serem removidos do estoque: ");
+                    produto.RemoverProduto(int.Parse(Console.ReadLine()));
+                }
+            }
+        }
+    }
+}
