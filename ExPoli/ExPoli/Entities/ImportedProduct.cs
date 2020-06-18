48559e19da9579718a24da1e4be47f502efb8fe5
Patrick Geovani <patrick.geovani357@hotmail.com>
2020-06-18 15:00:32 -0300

    Exercicio de Polimorfismo

---
+++ ExPoli/ExPoli/Entities/ImportedProduct.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExPoli.Entities
+{
+    class ImportedProduct : Product
+    {
+        public double CustomsFee { get; set; }
+
+        public ImportedProduct()
+        {
+        }
+
+        public ImportedProduct(string name, double price, double customsFee) : base(name, price)
+        {
+            CustomsFee = customsFee;
+        }
+
+        public double TotalPrice()
+        {
+            return Price + CustomsFee;
+        }
+
+        public sealed override string PriceTag()
+        {
+            return Name + " $ " + TotalPrice().ToString("F2") + "(Customs fee: $ " + CustomsFee.ToString("F2") + ")";
+        }
+    }
+}
---
+++ ExPoli/ExPoli/Program.cs
@@ -0,0 +1,53 @@
+﻿using ExPoli.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace ExPoli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>();
+
+            Console.Write("Enter the number of products: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Product #{i+1} data ");
+                Console.Write("Common, used or imported (c/u/i)? ");
+                char cui = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Price: ");
+                double price = double.Parse(Console.ReadLine());
+
+                if (cui == 'i')
+                {
+                    Console.Write("Customs fee: ");
+                    double customsFee = double.Parse(Console.ReadLine());
+
+                    products.Add(new ImportedProduct(name, price, customsFee));
+                }
+                else if (cui == 'u')
+                {
+                    Console.Write("Manufacture date (DD/MM/YYYY): ");
+                    DateTime manufactureDate = DateTime.Parse(Console.ReadLine());
+
+                    products.Add(new UsedProduct(name, price, manufactureDate));
+                }
+                else
+                {
+                    products.Add(new Product(name, price));
+                }
+            }
+
+            Console.WriteLine("\n PRICE TAGS:");
+            foreach (Product item in products)
+            {
+                Console.WriteLine(item.PriceTag());
+            }
+        }
+    }
+}
