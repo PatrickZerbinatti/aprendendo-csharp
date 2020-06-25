8784907fa331b48c43d0c71722255ce161742722
Patrick Geovani <patrick.geovani357@hotmail.com>
2020-06-25 12:21:55 -0300

    Ex de manipulacao de File, nao usei OO :/ mas ta bom

---
+++ ExFile/ExFile/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+
+namespace ExFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = Path.GetTempPath()+@"\teste\products.csv";
+            string directoryPath = Path.GetDirectoryName(sourcePath) + @"\out";
+            string targetPath = directoryPath + @"\summary.csv";
+
+            try
+            {
+                Directory.CreateDirectory(directoryPath);
+                
+                string[] lines = File.ReadAllLines(sourcePath);
+                using StreamWriter sw = File.CreateText(targetPath);
+                foreach (string line in lines)
+                {
+                    string[] columns = line.Split(",");
+
+                    string product = columns[0];
+
+                    double totalValue = double.Parse(columns[1], CultureInfo.InvariantCulture) * double.Parse(columns[2], CultureInfo.InvariantCulture);
+
+                    sw.WriteLine(product +","+ totalValue.ToString("F2",CultureInfo.InvariantCulture));
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Erro de IO: " + e.Message);
+            }
+        }
+    }
+}
