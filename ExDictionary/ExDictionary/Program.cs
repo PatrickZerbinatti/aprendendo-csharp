2ca7ed194740ebc1ea419a2e8dde87ac33c47e90
Patrick Geovani <patrick.geovani357@hotmail.com>
2020-06-28 16:23:40 -0300

    Exercicio de Dictionary, ta certinho

---
+++ ExDictionary/ExDictionary/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter file full path: "); string path = Console.ReadLine();
+            try { 
+                using (StreamReader sr = File.OpenText(path)) 
+                {
+                    Dictionary<string, int> candidatos = new Dictionary<string, int>();
+
+                    while (!sr.EndOfStream) 
+                    {
+                        string[] vetor = sr.ReadLine().Split(',');
+
+                        if (!candidatos.ContainsKey(vetor[0]))
+                        {
+                            candidatos[vetor[0]] = int.Parse(vetor[1]);
+                        }
+                        else
+                        {
+                            candidatos[vetor[0]] += int.Parse(vetor[1]);
+                        }
+                    }
+
+                    foreach (KeyValuePair<string,int> candidato in candidatos)
+                    {
+                        Console.WriteLine(candidato.Key + ", " + candidato.Value);
+                    }
+                } 
+            } catch (IOException e) 
+            { 
+                Console.WriteLine(e.Message); 
+            }
+        }
+    }
+}
