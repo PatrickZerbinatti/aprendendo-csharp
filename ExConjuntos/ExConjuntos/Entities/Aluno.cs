ac629666a32eb3bf45423cd281ff118d878203dc
Patrick Geovani <patrick.geovani357@hotmail.com>
2020-06-28 15:30:52 -0300

    Exercicio de conjuntos, eu fiz dando override no equal e gethashcode sem union.

---
+++ ExConjuntos/ExConjuntos/Entities/Aluno.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExConjuntos.Entities
+{
+    class Aluno
+    {
+        public int CodigoAluno { get; set; }
+
+        public Aluno(int codigoAluno)
+        {
+            CodigoAluno = codigoAluno;
+        }
+
+        public override int GetHashCode()
+        {
+            return CodigoAluno.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Aluno))
+            {
+                return false;
+            }
+            Aluno other = obj as Aluno;
+
+            return CodigoAluno.Equals(other.CodigoAluno);
+        }
+    }
+}
---
+++ ExConjuntos/ExConjuntos/Program.cs
@@ -0,0 +1,40 @@
+﻿using ExConjuntos.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace ExConjuntos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<Aluno> alunos = new HashSet<Aluno>();
+
+            Console.Write("How many students for course A?");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                alunos.Add(new Aluno(int.Parse(Console.ReadLine())));
+            }
+
+            Console.Write("How many students for course B?");
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                alunos.Add(new Aluno(int.Parse(Console.ReadLine())));
+            }
+
+            Console.Write("How many students for course C?");
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                alunos.Add(new Aluno(int.Parse(Console.ReadLine())));
+            }
+
+            Console.WriteLine("Total de estudantes: " + alunos.Count); 
+        }
+    }
+}
