6aa5922d1fd9717be75877b83d1df4fd9a489b9b
Patrick Geovani <patrick.geovani357@hotmail.com>
2020-08-12 10:07:32 -0300

    Exercicio de Web iniciado

---
+++ ExWeb/ExWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ExWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
